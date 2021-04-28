ae1829d5c6c988ea2deadb23ac2e90f8884e8327
kolzeq <diego.dupin@gmail.com>
2021-04-28 17:53:58 +0200

    test - test unification using test-marchine-script
    testing use connector-test-machine test script that permit to run the following tests :
    * MariaDB all supported version
    * Maxscale
    * MySQL
    * SkySQL
    
    for skysql, environement CONNECTOR_TEST_SECRET_KEY must be provided, or test will just be skipped

--- unittest/libmariadb/my_test.h
+++ unittest/libmariadb/my_test.h
@@ -73,7 +73,7 @@ if (IS_SKYSQL(hostname)) \
 #define SKIP_NOTLS
 #endif
 
-#define IS_MAXSCALE() (strcmp(getenv("srv"), "maxscale") || strcmp(getenv("srv") == "skysql-ha"))
+#define IS_MAXSCALE() (strcmp(getenv("srv"), "maxscale") != 0 || strcmp(getenv("srv") == "skysql-ha") != 0)
 #define SKIP_MAXSCALE \
 if (IS_MAXSCALE()) \
 { \
